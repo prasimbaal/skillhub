662ebd9908d52c7abd1b210734a4d81639dc3188
Merrycodesx <148143035+Merrycodesx@users.noreply.github.com>
2024-07-24 11:48:56 +0300

    common page files

---
+++ frontend/src/App.js
@@ -0,0 +1,75 @@
+import React from "react";
+import './index.css';
+import {Route, BrowserRouter as Router, Routes} from "react-router-dom";
+import Layout from "./components/common/Layout";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Blog from "./pages/Blog";
+import Courses from "./pages/Courses";
+import Instructor from "./pages/Instructor";
+import BlogSinglePage from "./components/common/BlogSinglePage";
+
+const App = () => {
+  return (
+    <>
+
+     <Router>
+        <Routes>
+          <Route 
+          path="/" 
+          element = {
+              <Layout>
+                <Home/>
+                </Layout>
+              }
+              />
+              <Route 
+          path="/about" 
+          element = {
+              <Layout>
+                <About/>
+                </Layout>
+              }
+              />
+              <Route 
+          path="/blog" 
+          element = {
+              <Layout>
+                <Blog/>
+                </Layout>
+              }
+              />
+              <Route 
+          path="/courses" 
+          element = {
+              <Layout>
+                <Courses/>
+                </Layout>
+              }
+              />
+              <Route 
+          path="/instructor" 
+          element = {
+              <Layout>
+                <Instructor/>
+                </Layout>
+              }
+              />
+        
+              
+              <Route 
+          path="/single-page" 
+          element = {
+              <Layout>
+                <BlogSinglePage/>
+                </Layout>
+              }
+              />
+
+        </Routes>
+     </Router>
+     </>
+  );
+};
+
+export default App;
---
+++ frontend/src/components/assets/data/dummydata.js
@@ -0,0 +1,134 @@
+export const LinkData = [
+    {
+      id: 1,
+      title: "Home",
+      url: "/",
+    },
+    {
+      id: 2,
+      title: "About",
+      url: "/about",
+    },
+    {
+      id: 3,
+      title: "courses",
+      url: "/courses",
+    },
+    {
+      id: 4,
+      title: "Instructor",
+      url: "/instructor",
+    },
+    {
+      id: 5,
+      title: "blog",
+      url: "/blog",
+    },
+    
+  ]
+  export const courses = [
+    {
+      id: 1,
+      cover: "../images/about.jpg",
+      title: "The Power of Podcast for Storytelling",
+    },
+    {
+      id: 2,
+      cover: "../images/course-4-1.jpg",
+      title: "Fashion and Luxury Fashion in a Changing",
+    },
+    {
+      id: 3,
+      cover: "../images/course-5-1.jpg",
+      title: "Creative writing through Storytelling",
+    },
+    {
+      id: 4,
+      cover: "../images/course-6-1.jpg",
+      title: "Improving The Accessibility Of Your Markdown",
+    },
+    {
+      id: 5,
+      cover: "../images/course11.jpg",
+      title: "Product Manager Learn the Skills & job.",
+    },
+    {
+      id: 6,
+      cover: "../images/course12.jpg",
+      title: "The business Intelligence analyst Course 2022",
+    },
+  ];
+
+  export const footerData =[
+    {
+        id: 1,
+        header: "Company",
+        subtitle:[
+            {
+                id:11,
+                header:"Contact",
+            },
+            {
+                id:12,
+                header:"Portfolio",
+            },
+            {
+                id:13,
+                header:"Blog",
+            },
+            {
+                id:14,
+                header:"Our Team",
+            },
+            {
+                id:13,
+                header:"Get In Touch",
+            },
+            {
+                id:14,
+                header:"FAQ",
+            },
+        ],
+    },
+    {
+        id: 2,
+        header: "Platform",
+        subtitle:[
+            {
+                id:11,
+                header:"Pricing",
+            },
+            {
+                id:12,
+                header:"blog",
+            },
+            {
+                id:13,
+                header:"Landing",
+            },
+            
+        ],
+    },
+    {
+        id: 3,
+        header: "subscribe",
+        subtitle:[
+            {
+                id:11,
+                header:"Contact",
+            },
+            {
+                id:12,
+                header:"about us",
+            },
+            {
+                id:13,
+                header:"reviews",
+            },
+            {
+                id:14,
+                header:"services",
+            },
+        ],
+    },
+  ];
