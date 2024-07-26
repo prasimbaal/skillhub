9b3831de9769eb6ab717e9a7b98ef1e22e7c44be
prasimbaal <prasimbaal@gmail.com>
2024-07-25 20:11:37 -0700

    rest api and serializers

--- userMgmt/views.py
+++ userMgmt/views.py
@@ -7,9 +7,51 @@ from django.contrib.auth.hashers import make_password
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import login, logout, authenticate
 
+# rest_framework imports
+from rest_framework.authentication import SessionAuthentication
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .serializers import UserSerializer, UserLoginSerializer, UserRegisterSerializer
+from rest_framework import status, permissions
 
-# Create your views here.
+###########################api views ##############################
+from rest_framework.generics import RetrieveUpdateDestroyAPIView
+from django.contrib.auth.models import User
 
+class UserRegisterView(APIView):
+    permission_classes = (permissions.AllowAny,)
+    
+    def post(self, request, format=None):
+        serializer = UserRegisterSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            if user:
+                json = serializer.data
+                return Response(json, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class UserLoginView(APIView):
+    permission_classes = (permissions.AllowAny,)
+    authentication_classes = (SessionAuthentication,)
+    def post(self, request):
+        serializer = UserLoginSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            json = serializer.data
+            return Response(json, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+ 
+
+class logoutView(APIView):
+    def post(self, request, format=None):
+        logout(request)
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+class profileView(APIView):
+    permission_classes = (permissions.IsAuthenticated, )
+    authentication_classes = (SessionAuthentication, )
+    def get(self, request, format=None):
+        json = UserSerializer(request.user).data
+        return Response(json, status=status.HTTP_200_OK)
 
 
 def index(request):
@@ -17,45 +59,51 @@ def index(request):
     # path = {'img_path':"skillUp\home\img", }
     return HttpResponse(template.render() )
 
+
+
+
+
+
+##############################template views ##############################
 # def reg(request):
 #     template = loader.get_template('landing/register.html')
 #     return HttpResponse(template.render())
 
-def userReg(request):
-    registered = False
-    # form = UserCreationForm()
-    if request.method=="POST":
-        form = SignupForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            user.password = make_password('password')
-            User.save(user)
-            registered = True
-            login(User)
-            return redirect('index')
-        else:
-            print(form.errors)
-    else:
-        form = SignupForm()
+# def userReg(request):
+#     registered = False
+#     # form = UserCreationForm()
+#     if request.method=="POST":
+#         form = SignupForm(request.POST)
+#         if form.is_valid():
+#             user = form.save()
+#             user.password = make_password('password')
+#             User.save(user)
+#             registered = True
+#             login(User)
+#             return redirect('index')
+#         else:
+#             print(form.errors)
+#     else:
+#         form = SignupForm()
 
-    context = {'form':form, 'registered':registered}
-    return render(request,'signup.html', context)
+#     context = {'form':form, 'registered':registered}
+#     return render(request,'signup.html', context)
 
 
-def user_login(request):
-    if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
+# def user_login(request):
+#     if request.method == 'POST':
+#         username = request.POST.get('username')
+#         password = request.POST.get('password')
 
-        user = authenticate(username=username, password=password)
-        if user:
-            if user.is_active():
-                # login(request, user)
-                return HttpResponseRedirect('index')
-            else:
-                HttpResponse("ACCOUNT NOT ACTIVE")
-        else:
-            print("not a user trying to login")
-            return HttpResponse("INVALID LOGIN DETAILS")
-    else:
-        return render(request, 'login.html', {})
+#         user = authenticate(username=username, password=password)
+#         if user:
+#             if user.is_active():
+#                 # login(request, user)
+#                 return HttpResponseRedirect('index')
+#             else:
+#                 HttpResponse("ACCOUNT NOT ACTIVE")
+#         else:
+#             print("not a user trying to login")
+#             return HttpResponse("INVALID LOGIN DETAILS")
+#     else:
+#         return render(request, 'login.html', {})
