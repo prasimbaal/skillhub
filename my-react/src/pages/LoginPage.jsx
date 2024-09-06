3e39e6091753e4e8c005500b00bf923a02e14efa
prasimbaal <prasimbaal@gmail.com>
2024-09-05 17:14:52 -0700

    cors edit

--- my-react/src/pages/LoginPage.jsx
+++ my-react/src/pages/LoginPage.jsx
@@ -4,6 +4,7 @@ import axios from 'axios';
 axios.defaults.xsrfCookieName = 'csrftoken';
 axios.defaults.xsrfHeaderName = 'X-CSRFToken';
 axios.defaults.withCredentials = true;
+// axios.defaults.headers.common['Set-Cookie'] = 'sessionid=...; SameSite=None; Secure';
 
 const client = axios.create({
   baseURL: "http://127.0.0.1:8000"
